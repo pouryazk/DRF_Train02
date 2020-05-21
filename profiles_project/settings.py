9edd48997e73395097651d9fcd319e0000d340d2
zakeri ,Pooria <pooria.zakeri@eniac-tech.local>
2020-05-21 18:47:39 +0430

    updated allowed host

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,8 +25,10 @@ SECRET_KEY = 'spda4-qn5j+=blcqptry+3nl^@5ctx28l5excgiv_)l1qio+5r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    'ec2-18-132-9-47.eu-west-2.compute.amazonaws.com',
+    '127.0.0.1',
+                ]
 
 # Application definition
 
